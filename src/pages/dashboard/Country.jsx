6af8bdb014a772b9fb4b3118e021d8432c258aba
abhinand-vp <abhinandvpellath@gmail.com>
2024-05-26 15:51:54 +0530

    country started

--- src/pages/dashboard/Country.jsx
+++ src/pages/dashboard/Country.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import { useState } from 'react'
 
 const Country = () => {
+  const [edit, setEdit] = useState(false)
   return (
-    <div>Country</div>
+    <div className='country_table'>
+      {edit &&
+        <h1>Modal</h1>
+      }
+      <h3>Country Details</h3>
+      <table>
+        <tr className='heading_border'>
+          <th>sL.No</th>
+          <th>Country</th>
+          <th>Code</th>
+          <th>Edit</th>
+          <th>View</th>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>India</td>
+          <td>IND</td>
+          <td><img src="/public/icons/Create.svg" onClick={() => setEdit(!edit)} alt="" /></td>
+          <td><img src="/public/icons/Eye.svg" alt="" /></td>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>India</td>
+          <td>IND</td>
+          <td><img src="/public/icons/Create.svg" alt="" /></td>
+          <td><img src="/public/icons/Eye.svg" alt="" /></td>
+        </tr>
+      </table>
+    </div>
   )
 }
 
